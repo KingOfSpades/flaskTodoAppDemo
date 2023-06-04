f2514fe5ed0f398394168be17057295c4faa5e04
Christian ∆ Benstein <Christian.Benstein@Topicus.nl>
2023-06-04 11:01:03 +0200

    Reworked methods

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask_wtf import FlaskForm
 from wtforms import IntegerField, StringField, SubmitField
 from flask_sqlalchemy import SQLAlchemy
@@ -23,31 +23,61 @@ class Todo(db.Model):
 
 # Todo form for adding items
 class TodoForm(FlaskForm):
-  id = IntegerField("Id")
   todo = StringField("Todo")
   submit = SubmitField("Add")
 
-@app.route('/', methods=["GET", "POST"])
+@app.route('/', methods=["GET"])
 def index():
-    # if todo is filled in a POST request add it to the db
-    if 'todo' in request.form:
-        with app.app_context():
-            db.session.add(
-               Todo(
-                todo_text=request.form['todo'],
-                )
-            )
-            db.session.commit()
-
-    # if id is filled in a POST request delete it from the db
-    if 'id' in request.form:
-        with app.app_context():
-            db.session.remove(request.form['id'],
-            )
-            db.session.commit()
-
     return render_template(
        'index.html',
         todos=Todo.query.all(),
         template_form=TodoForm()
         )
+
+@app.route('/add', methods=["POST"])
+def add():
+    # if todo is filled in a POST request add it to the db
+    if 'todo' in request.form:
+      with app.app_context():
+          db.session.add(
+            Todo(
+              todo_text=request.form['todo'],
+              )
+          )
+          db.session.commit()
+    return redirect('/')
+
+@app.route('/update/<int:id>')
+def update(id):
+  # Let's take the id passed in the URL and toggle the done status
+  with app.app_context():
+      # Get the todo or return a 404
+      todo = Todo.query.get_or_404(id)
+
+      # Let's check if our todo is done or not:
+      if todo.completed:
+         todo.completed = False
+      else:
+         todo.completed = True
+
+      # Ask the db to update the todo we got
+      try:
+        db.session.commit()
+      except:
+        return "Something went wrong with our db"
+  return redirect('/')
+
+@app.route('/delete/<int:id>')
+def delete(id):
+  # Let's take the id passed in the URL and try to delete the todo
+  with app.app_context():
+      # Get the todo or return a 404
+      todo = Todo.query.get_or_404(id)
+
+      # Ask the db to delete the todo we got
+      try:
+        db.session.delete(todo)
+        db.session.commit()
+      except:
+        return "Something went wrong with our db"
+  return redirect('/')
